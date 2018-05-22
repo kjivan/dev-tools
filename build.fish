922e17850245bd03144e82e52ca0d1c10463c395
Kavi Jivan <kavi.jivan@viavisolutions.com>
2018-05-22 16:16:36 -0400

    Updates build script to just build and not test

--- build.fish
+++ build.fish
@@ -1,13 +1,20 @@
 #!/usr/bin/env fish
 
+set -xg DISTCC_DISABLE 1
+
 if not test -d build
     mkdir build
 end
 
 pushd build
-make
+cmake .. -DCMAKE_INSTALL_PREFIX=../install $argv
+
 if test $status -eq 0
-    set -xg CTEST_OUTPUT_ON_FAILURE TRUE
-    ctest | ccze -A
+    make
 end
+
+set return_code $status
+
 popd
+
+exit $return_code
