60d5d5882b61190836340dd40eebbc499aa4dee0
Kavi Jivan <kavi.jivan@viavisolutions.com>
2019-01-31 22:38:52 -0500

    Fix exit status for build script

--- build.fish
+++ build.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+set success 0
+set failure 0
+
 set -xg DISTCC_DISABLE 1
 
 if not test -d build
@@ -9,12 +12,17 @@ end
 pushd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install -DBUILD_ENV=NATIVE $argv
 
-if test $status -eq 0
+set cmake_status $status
+echo $cmake_status
+if test $cmake_status -eq $success
     make
+    set make_status $status
 end
-
-set return_code $status
 
 popd
 
-exit $return_code
+if set -q make_status -a $make_status -eq $success
+    exit
+else
+    false
+end
